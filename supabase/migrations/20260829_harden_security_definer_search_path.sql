808884862185d46426a7a948f46293d64902a992
Fantommyn <z7pwz7sfdb@privaterealy.appleid.com>
2026-08-29 22:07:04 +0200

    Harden security definer search paths

---
+++ supabase/migrations/20260829_harden_security_definer_search_path.sql
@@ -0,0 +1,15 @@
+﻿begin;
+
+alter function public.consume_demo_rate_limit(
+  text,
+  integer,
+  integer
+)
+set search_path = '';
+
+alter function public.mark_aegris_alert_read(
+  bigint
+)
+set search_path = '';
+
+commit;
